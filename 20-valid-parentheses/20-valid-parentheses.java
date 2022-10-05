2cf2e8abf905f23caabf9d80df6c3024bb28ed7a
TheCoder_12 <46420386+sukritigosain@users.noreply.github.com>
2022-10-05 21:10:48 +0530

    Time: 3 ms (70.79%), Space: 41.6 MB (80.37%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.charAt(0)==')'||s.charAt(0)==(']') ||s.charAt(0)=='}')
+            return false;
+        
+        Stack<Character> open=new Stack<Character>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' ||s.charAt(i)=='{')
+            {
+                open.push(s.charAt(i));
+            }
+            else {
+                if(s.charAt(i)==')' )
+                {
+                    if(!open.empty() && open.peek()=='(')
+                        open.pop();
+                    else
+                        return false;
+                }
+                if(s.charAt(i)==']')
+                {
+                   if(!open.empty() && open.peek()=='[')
+                        open.pop();
+                    else
+                        return false; 
+                }
+                if(s.charAt(i)=='}')
+                {
+                    if(!open.empty() && open.peek()=='{')
+                        open.pop();
+                    else
+                        return false; 
+                }
+            }
+       
+        }
+        if(!open.empty())
+        {
+            return false;
+        }
+        return true;
+       
+}
+}
